222cee095fa481ba3dc75d4992a6f2d17ed919e4
Klim Orlov <flameko-dan@yandex.ru>
2022-09-03 09:59:32 +0300

    mongo server

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const options = {}; // TODO
 
-mongoose.connect('mongodb://localhost:27017/mestodb', options, (err) => {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', options, (err) => {
   if (err) console.log(err);
   else console.log('database connection');
 });
